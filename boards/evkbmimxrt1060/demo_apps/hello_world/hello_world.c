f0124f7f70c0ad5e431361bb4f61c3a850b6df49
Jay Heng <hengjie1989@foxmail.com>
2024-12-16 22:46:26 +0800

    [RT1060] Initial code flow - HWF
    - Refer to power switch demo
    - Added one HWF case (from CAS)
    
    Signed-off-by: Jay Heng <hengjie1989@foxmail.com>

--- boards/evkbmimxrt1060/demo_apps/hello_world/hello_world.c
+++ boards/evkbmimxrt1060/demo_apps/hello_world/hello_world.c
@@ -22,12 +22,21 @@
  * Prototypes
  ******************************************************************************/
 
+typedef enum _hw_failure_mode
+{
+    HW_FailureModeLed = 0,
+    HW_FailureModeWfi,
+    HW_FailureModeAccessPeriph,
+
+    HW_FailureModeEnd = HW_FailureModeAccessPeriph
+} hw_failure_mode_t;
+
 /*******************************************************************************
  * Variables
  ******************************************************************************/
 volatile uint32_t g_systickCounter;
-/* The PIN status */
-volatile bool g_pinSet = false;
+
+static hw_failure_mode_t s_targetFailureMode;
 
 /*******************************************************************************
  * Code
@@ -48,12 +57,96 @@ void SysTick_DelayTicks(uint32_t n)
     }
 }
 
+void HWF_BlinkyLed(void)
+{
+    /* Set systick reload value to generate 1ms interrupt */
+    if (SysTick_Config(SystemCoreClock / 1000U))
+    {
+        while (1)
+        {
+        }
+    }
+    uint8_t g_pinSetCnt = 0;
+    while (1)
+    {
+        /* Delay 1000 ms */
+        SysTick_DelayTicks(1000U);
+        PRINTF("Toggle LED once.\r\n");
+        if (g_pinSetCnt % 2)
+        {
+            GPIO_PinWrite(EXAMPLE_LED_GPIO, EXAMPLE_LED_GPIO_PIN, 0U);
+        }
+        else
+        {
+            GPIO_PinWrite(EXAMPLE_LED_GPIO, EXAMPLE_LED_GPIO_PIN, 1U);
+        }
+        g_pinSetCnt++;
+        if (g_pinSetCnt > 8)
+        {
+            SysTick->CTRL &= ~SysTick_CTRL_ENABLE_Msk;
+            break;
+        }
+    }
+}
+
+void HWF_EnterWFI(void)
+{
+    __asm volatile("dsb");
+    /* wait for interrupt: the next interrupt will wake us up */
+    __asm volatile("wfi");
+    __asm volatile("isb");
+}
+
+void HWF_AccessPeriph(void)
+{
+    //PRINTF("%x\r\n", CAN1->MCR);
+    //PRINTF("%x\r\n", CAN1->CTRL1);
+    //PRINTF("%x\r\n", CAN1->TIMER);
+    
+    PRINTF("%x\r\n", CAN1->RXMGMASK);
+    PRINTF("%x\r\n", CAN1->RX14MASK);
+    PRINTF("%x\r\n", CAN1->RX15MASK);
+  
+    //PRINTF("%x\r\n", CAN1->ECR);
+    //PRINTF("%x\r\n", CAN1->ESR1);
+    //PRINTF("%x\r\n", CAN1->IMASK2);
+    //PRINTF("%x\r\n", CAN1->IMASK1);
+    //PRINTF("%x\r\n", CAN1->IFLAG2);
+
+    /*
+    uint32_t *ptr = (uint32_t *)CAN1;
+    for (uint32_t i = 4; i < 30; i++)
+    {
+        PRINTF("%x\r\n", *(ptr+i));
+    }
+   */
+}
+
+void APP_FailureModeSelection(hw_failure_mode_t targetFailureMode)
+{
+    switch (targetFailureMode)
+    {
+        case HW_FailureModeLed:
+            HWF_BlinkyLed();
+            break;
+        case HW_FailureModeWfi:
+            HWF_EnterWFI();
+            break;
+        case HW_FailureModeAccessPeriph:
+            HWF_AccessPeriph();
+            break;
+        default:
+            assert(false);
+            break;
+    }
+}
+
 /*!
  * @brief Main function
  */
 int main(void)
 {
-    //char ch;
+    char ch;
 
     /* Init board hardware. */
     BOARD_ConfigMPU();
@@ -65,31 +158,27 @@ int main(void)
     SystemCoreClockUpdate();
     CLOCK_EnableClock(kCLOCK_Trace);
 
-    /* Set systick reload value to generate 1ms interrupt */
-    if (SysTick_Config(SystemCoreClock / 1000U))
-    {
-        while (1)
-        {
-        }
-    }
+    PRINTF("\r\n########## HW failure Demo ###########\n\r\n");
+    PRINTF("\r\nSelect the desired operation \n\r\n");
+    PRINTF("Press %c for enter case: LED blinky mode\r\n", (uint8_t)'A' + (uint8_t)HW_FailureModeLed);
+    PRINTF("Press %c for enter case: CPU WFI mode\r\n", (uint8_t)'A' + (uint8_t)HW_FailureModeWfi);
+    PRINTF("Press %c for enter case: Peripheral access before clocking mode\r\n", (uint8_t)'A' + (uint8_t)HW_FailureModeAccessPeriph);
+    PRINTF("\r\nWaiting for HW failure case select...\r\n\r\n");
 
     while (1)
     {
-        //ch = GETCHAR();
-        //PUTCHAR(ch);
+        ch = GETCHAR();
+        if ((ch >= 'a') && (ch <= 'z'))
+        {
+            ch -= 'a' - 'A';
+        }
+        s_targetFailureMode = (hw_failure_mode_t)(ch - 'A');
 
-        /* Delay 1000 ms */
-        SysTick_DelayTicks(1000U);
-        PRINTF("hello world.\r\n");
-        if (g_pinSet)
+        if (s_targetFailureMode <= HW_FailureModeEnd)
         {
-            GPIO_PinWrite(EXAMPLE_LED_GPIO, EXAMPLE_LED_GPIO_PIN, 0U);
-            g_pinSet = false;
+            APP_FailureModeSelection(s_targetFailureMode);
         }
-        else
-        {
-            GPIO_PinWrite(EXAMPLE_LED_GPIO, EXAMPLE_LED_GPIO_PIN, 1U);
-            g_pinSet = true;
-        }
+
+        PRINTF("\r\nNext loop\r\n");
     }
 }
